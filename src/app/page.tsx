2d5d7cfdbb0f544cd9f0dccbe92e1e5cd23b5a95
most-x <web-master@mostx.co.kr>
2024-04-09 13:32:56 +0900

    costco commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ function Home() {
 				<Link href="/homeShopping" legacyBehavior>
 					<a className='btn blue_back'>홈쇼핑 방송 정보</a>
 				</Link>
-				<Link href="http://31.152.254.254:5555/costco/list" legacyBehavior>
+				<Link href="" legacyBehavior>
 					<a className='btn blue_back'>코스트코 리퍼브 상품</a>
 				</Link>
 			</div>
