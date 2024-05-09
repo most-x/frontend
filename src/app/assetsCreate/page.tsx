85c1110c73f3bb4b57b56b64f0689579c27751aa
most-x <web-master@mostx.co.kr>
2024-05-09 16:53:54 +0900

    update4

--- src/app/assetsCreate/page.tsx
+++ src/app/assetsCreate/page.tsx
@@ -75,7 +75,7 @@ export type assetCreateType = {
 						console.log(response);
 						alert('자산등록이 되었습니다.');
 						//location.href='//support.mostx.co.kr/assetsList';
-						redirect("/assetsList");
+						//redirect("/assetsList");
 					})
 					.catch(function(error) {
 						console.log(error);
