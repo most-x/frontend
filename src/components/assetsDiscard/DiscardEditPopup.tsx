7e18f2a4ff4426cf524e582efcba7fae9a0a6b0a
most-x <web-master@mostx.co.kr>
2024-06-04 09:45:01 +0900

    update22

--- src/components/assetsDiscard/DiscardEditPopup.tsx
+++ src/components/assetsDiscard/DiscardEditPopup.tsx
@@ -224,10 +224,10 @@ export default function DiscardEditPopup({
                                     onChange={handleEditPopupChange}
                                     disabled={
                                         editData.modifiedYn === 'Y'
-                                            ? false
+                                            ? true
                                             : editData.assetStatus === '정상'
-                                            ? false
-                                            : true
+                                            ? true
+                                            : false
                                     }
                                 />
                                 <span>&nbsp;감가 상가 진행 전까지 일자 변경이 가능 합니다.</span>
